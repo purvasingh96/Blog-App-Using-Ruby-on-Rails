e2577429b34460aa65fe3e4325fee9b5a93bac64
psingh359 <singhpurva2906@gmail.com>
2023-05-13 19:27:10 -0400

    Adds password confirmation validation and TOS acceptance

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,9 @@
 class User < ApplicationRecord
-    def password_requirements_are_met
-        rules = {
-          " must contain at least one lowercase letter"  => /[a-z]+/,
-          " must contain at least one uppercase letter"  => /[A-Z]+/,
-          " must contain at least one digit"             => /\d+/,
-          " must contain at least one special character" => /[^A-Za-z0-9]+/
-        }
-      
-        rules.each do |message, regex|
-          errors.add( :password, message ) unless @password.match( regex )
-        end
-      end
-
+    
     validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, confirmation: true
+
+    validate :tos_aggrement_acceptance
     validate :password_requirements_are_met
 
     def password
@@ -26,5 +17,22 @@ class User < ApplicationRecord
     
     def is_password?(raw)
         BCrypt::Password.new(password_digest).is_password?(raw)
+    end
+
+    def tos_aggrement_acceptance
+        errors.add(:terms_of_service,  " must be accepted before registering.") unless @terms_of_service == true
+    end
+
+    def password_requirements_are_met
+        rules = {
+          " must contain at least one lowercase letter"  => /[a-z]+/,
+          " must contain at least one uppercase letter"  => /[A-Z]+/,
+          " must contain at least one digit"             => /\d+/,
+          " must contain at least one special character" => /[^A-Za-z0-9]+/
+        }
+      
+        rules.each do |message, regex|
+          errors.add( :password, message ) unless @password.match( regex )
+        end
     end
 end
